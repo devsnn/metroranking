5724acbf348beacaf9f062b8c098673414e6b44e
devsnn <sedurn4@naver.com>
2015-04-03 13:35:19 +0900

    no message

--- MetroRanking/ViewController.swift
+++ MetroRanking/ViewController.swift
@@ -39,6 +39,8 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegat
         
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
+        // test2
+        
         
     }
     
